a1fee99eb189ba4216b083b2b1a6c3c026d47615
renjuzac <renjuzac@gmail.com>
2018-05-13 06:34:54 +0000

    basic functions

---
+++ calc.py
@@ -0,0 +1,11 @@
+
+def eval(expr):
+    # tokenize
+    # parse
+    return "parse result"
+    
+def tokenize(expr):
+    pass
+
+def parse(tokens):
+    pass
---
+++ test_calc.py
@@ -0,0 +1,11 @@
+from calc import *
+
+def test_eval():
+    expr = "1+1"
+    assert eval(expr) == 2
+
+def test_tokenize():
+    pass
+
+def test_parse():
+    pass
